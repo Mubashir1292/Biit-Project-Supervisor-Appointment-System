8bfa35e0e5453fa0f302069d26135478abee506e
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-04-23 13:29:43 +0500

    Break time

--- src/pages/Project_Commetiee/Supervisor-Requests/SupervisorRequests.jsx
+++ src/pages/Project_Commetiee/Supervisor-Requests/SupervisorRequests.jsx
@@ -1,7 +1,18 @@
 import React from "react";
-
+import biitSAS from "../../../assets/extra/biitSAS.png";
 function SupervisorRequests() {
-  return <div>SupervisorRequests</div>;
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="flex  justify-center">
+          <img src={biitSAS} alt="Biit Sas Logo" />
+        </div>
+        <h1 className="text-2xl text-center font-bold text-green-600">
+          Supervising Requests
+        </h1>
+      </div>
+    </>
+  );
 }
 
 export default SupervisorRequests;
