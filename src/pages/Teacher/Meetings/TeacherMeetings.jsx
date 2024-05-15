9ccbc8626bcda65bdfef5f63ba9f33972de17949
Mubashir1292 <161022598+Mubashir1292@users.noreply.github.com>
2024-05-15 21:15:07 +0500

    Meeting Module Approved

--- src/pages/Teacher/Meetings/TeacherMeetings.jsx
+++ src/pages/Teacher/Meetings/TeacherMeetings.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import moment from "moment";
+import AddMeeting from "./AddMeeting";
+const localizer = momentLocalizer(moment);
 
 function TeacherMeetings() {
-  return <div>TeacherMeetings</div>;
+  const [showMeetingModal, setShowMeetingModal] = useState(false);
+  const [selectedSlot, setSelectedSlot] = useState();
+  const [eventsList, setEventsList] = useState([
+    {
+      title: "Event 1",
+      start: new Date(),
+      end: new Date(new Date().setHours(new Date().getHours() + 1)),
+    },
+    {
+      title: "Event 2",
+      start: new Date(new Date().setDate(new Date().getDate() + 1)),
+      end: new Date(
+        new Date(new Date().setDate(new Date().getDate() + 1)).setHours(
+          new Date().getHours() + 1
+        )
+      ),
+    },
+  ]);
+
+  const handleSelectSlot = (slotInfo) => {
+    setSelectedSlot(slotInfo);
+    setShowMeetingModal(!showMeetingModal);
+    console.log(slotInfo);
+  };
+
+  return (
+    <div>
+      <Calendar
+        localizer={localizer}
+        events={eventsList}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        selectable
+        onSelectSlot={handleSelectSlot}
+      />
+      {showMeetingModal ? (
+        <>
+          <AddMeeting
+            show={showMeetingModal}
+            onHide={() => setShowMeetingModal(false)}
+            slot={selectedSlot}
+          />
+        </>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
 export default TeacherMeetings;
--- src/pages/Teacher/Requests/Requests.jsx
+++ src/pages/Teacher/Requests/Requests.jsx
@@ -1,29 +1,119 @@
-import React from "react";
+import React, { useState } from "react";
 import BiitSAS from "../../../assets/extra/biitSAS.png";
 import Card from "react-bootstrap/Card";
+import Table from "react-bootstrap/Table";
+import ListGroup from "react-bootstrap/ListGroup";
+import man2 from "../../../assets/extra/man2.jpg";
+import man from "../../../assets/extra/man.png";
+import { Button } from "react-bootstrap";
 function Requests() {
+  const [groupDetail, setGroupDetail] = useState({
+    groupImage: man2,
+    projectTitle: "Ai Health Engine",
+    groupCGPA: 2.9,
+    projectDescription: "Health Care of the People",
+    groupMembers: [
+      {
+        regNo: "2020-Arid-3675",
+        name: "Mubashir Liaqat",
+        Technology: "React-Js",
+        Cgpa: 2.9,
+        semester: "BSCS8",
+        section: "A",
+        grade: "B",
+        image: man2,
+      },
+      {
+        regNo: "2020-Arid-4224",
+        name: "Touseef Sajjad",
+        Technology: "Flutter",
+        Cgpa: 2.9,
+        semester: "BSCS8",
+        section: "A",
+        grade: "A",
+        image: man,
+      },
+      {
+        regNo: "2020-Arid-3677",
+        name: "Usama Ijaz",
+        Technology: "React-Native",
+        Cgpa: 2.9,
+        semester: "BSCS8",
+        section: "A",
+        grade: "B",
+        image: man,
+      },
+    ],
+  });
+  const [tableHeadings, setTableHeading] = useState([
+    "image",
+    "Reg#",
+    "Name",
+    "Technology",
+    "CGPA",
+    "Semester",
+    "Section",
+    "Grade",
+  ]);
   return (
-    <>
-      <div className="flex flex-col">
-        <img src={BiitSAS} alt="Biit SAS" className="flex self-center" />
-        <div className="flex justify-center">
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">
-                Card Subtitle
-              </Card.Subtitle>
-              <Card.Text>
-                Some quick example text to build on the card title and make up
-                the bulk of the card's content.
-              </Card.Text>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Another Link</Card.Link>
-            </Card.Body>
-          </Card>
-        </div>
+    <React.Fragment>
+      <div className="flex flex-col justify-center items-center">
+        <img src={BiitSAS} alt="Biit SAS" />
+        <Card>
+          {/* group Image */}
+          <div className="w-2/6 flex self-center rounded-full mt-1">
+            <Card.Img src={groupDetail.groupImage} alt={`${groupDetail}`} />
+          </div>
+          <Card.Body>
+            <Card.Title className="text-center text-sm">
+              {groupDetail.projectTitle}
+            </Card.Title>
+            <Card.Text className="text-center text-xs">
+              {groupDetail.projectDescription}
+            </Card.Text>
+            <Card.Subtitle className="flex justify-center items-center space-x-2">
+              <span className="text-sm">Group CGPA :</span>
+              <span className="text-sm">{groupDetail.groupCGPA}</span>
+            </Card.Subtitle>
+            <Table responsive bordered hover>
+              <thead>
+                <tr>
+                  <th>#</th>
+                  {tableHeadings.map((item, index) => (
+                    <th key={index}>{item}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {groupDetail.groupMembers.map((item, index) => (
+                  <tr key={index} className="cursor-default">
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        src={item.image}
+                        alt={`${item.name}`}
+                        className="w-5"
+                      />
+                    </td>
+                    <td>{item.regNo}</td>
+                    <td>{item.name}</td>
+                    <td>{item.Technology}</td>
+                    <td>{item.Cgpa}</td>
+                    <td>{item.grade}</td>
+                    <td>{item.semester}</td>
+                    <td>{item.section}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </Card.Body>
+          <div className="flex justify-center items-center space-x-3 mt-0 mb-2">
+            <Button variant="success">Accept</Button>
+            <Button variant="secondary">Reject</Button>
+          </div>
+        </Card>
       </div>
-    </>
+    </React.Fragment>
   );
 }
 
